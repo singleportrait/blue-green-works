86f8f47a86e921486f56f8eeb2bcd86dca4249f3
Sanity.io <hello@sanity.io>
2021-04-13 17:41:15 -0600

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,57 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-portfolio'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '60762c1774eee51314ae8445',
+                  title: 'Sanity Studio',
+                  name: 'sanity-gatsby-portfolio-studio-cxhvv8o9',
+                  apiId: '0d9228cb-a2d5-4d94-abc1-110d11474e6c'
+                },
+                {
+                  buildHookId: '60762c18546dc6128a477380',
+                  title: 'Portfolio Website',
+                  name: 'sanity-gatsby-portfolio-web-upo7qbx8',
+                  apiId: '5bb61685-1db2-4ae9-a6e8-5aeafb7dcc09'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/singleportrait/sanity-gatsby-portfolio',
+            category: 'Code'
+          },
+          {
+            title: 'Frontend',
+            value: 'https://sanity-gatsby-portfolio-web-upo7qbx8.netlify.app',
+            category: 'apps'
+          }
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent projects', order: '_createdAt desc', types: ['sampleProject']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'hontxz4x',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+  }
+}
