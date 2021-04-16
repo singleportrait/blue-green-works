7da633b6cc0f5596c0df5d71ea5b1449efd68db9
Jenn Scheer <singleportrait@gmail.com>
2021-04-16 18:41:48 -0400

    Add Gatsby dynamic image packages, and Sass support

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -12,6 +12,9 @@ module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-sanity',
       options: {
