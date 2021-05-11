e6561bdc144650bc6e3ccee9f4647cbd3b398a3e
Jenn Scheer <singleportrait@gmail.com>
2021-05-11 08:27:23 -0700

    Update product to pull series name when generating slug

--- studio/schemas/documents/product.js
+++ studio/schemas/documents/product.js
@@ -1,19 +1,35 @@
+import client from 'part:@sanity/base/client';
+
 export default {
   name: 'product',
   title: 'Product',
   type: 'document',
   fields: [
     {
+      name: 'series',
+      type: 'reference',
+      title: 'Series (Optional)',
+      to: [{type: 'series'}]
+    },
+    {
       name: 'title',
       title: 'Title',
       type: 'string',
+      description: "Don't include the name of the series in the title, the front-end will do that",
+      validation: Rule => Rule.required(),
     },
     {
       name: 'slug',
       title: 'Slug',
       type: 'slug',
       options: {
-        source: 'title',
+        source: async (doc) => {
+          if (!doc.series) {
+            return doc.title;
+          }
+          const series = await client.getDocument(doc.series._ref);
+          return `${series.title} ${doc.title}`;
+        },
         maxLength: 96
       }
     },
@@ -21,6 +37,13 @@ export default {
       name: 'description',
       title: 'Description',
       type: 'simplePortableText',
+    },
+    {
+      name: 'images',
+      type: 'array',
+      title: 'Images',
+      of: [{ type: 'figure' }],
+      validation: Rule => Rule.error('You must have at least 1 image').min(1)
     },
     {
       name: 'tearSheet',
@@ -47,23 +70,25 @@ export default {
       type: 'simplePortableText',
     },
     {
-      name: 'images',
-      type: 'array',
-      title: 'Images',
-      of: [{ type: 'figure' }],
-      validation: Rule => Rule.error('You must have at least 1 image').min(1)
-    },
-    {
-      name: 'series',
-      type: 'reference',
-      title: 'Series (Optional)',
-      to: [{type: 'series'}]
-    },
-    {
       name: 'category',
       type: 'reference',
       title: 'Category (Optional)',
       to: [{type: 'category'}]
     }
-  ]
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      series: 'series.title',
+      media: 'images.0.image',
+    },
+    prepare(selection) {
+      const {title, series, media} = selection;
+      return {
+        title: title,
+        subtitle: series ? `${series} Series` : '',
+        media: media
+      }
+    }
+  }
 }
