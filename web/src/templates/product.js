6aa100841688ff014235dab3c688c497da00c6e0
Jenn Scheer <singleportrait@gmail.com>
2021-05-12 20:05:49 -0700

    Pull in the first image (horizontal for now), and clean up missing keys

--- web/src/templates/product.js
+++ web/src/templates/product.js
@@ -11,6 +11,51 @@ import Button from '../components/button';
 import * as styles from './product.module.scss';
 import { cn } from "../lib/helpers";
 
+export const imageQuery = graphql`
+  fragment ImagesQuery on SanityProduct {
+    firstImage {
+      _key
+      image {
+        asset {
+          _id
+          gatsbyImageData(fit: FILLMAX)
+        }
+      }
+      alt
+    }
+    images {
+      _key
+      image {
+        crop {
+          _key
+          _type
+          top
+          bottom
+          left
+          right
+        }
+        asset {
+          gatsbyImageData(fit: FILLMAX)
+          url
+          metadata {
+            dimensions {
+              aspectRatio
+            }
+            palette {
+              dominant {
+                foreground
+              }
+            }
+          }
+        }
+        _key
+      }
+      alt
+      caption
+    }
+  }
+`;
+
 export const query = graphql`
   query ProductTemplateQuery($id: String!) {
     site: sanitySiteSettings(_id: { regex: "/siteSettings/" }) {
@@ -20,36 +65,7 @@ export const query = graphql`
     product: sanityProduct(id: { eq: $id }) {
       id
       title
-      images {
-        _key
-        image {
-          crop {
-            _key
-            _type
-            top
-            bottom
-            left
-            right
-          }
-          asset {
-            gatsbyImageData(fit: FILLMAX)
-            url
-            metadata {
-              dimensions {
-                aspectRatio
-              }
-              palette {
-                dominant {
-                  foreground
-                }
-              }
-            }
-          }
-          _key
-        }
-        alt
-        caption
-      }
+      ...ImagesQuery
       options {
         _id
         title
@@ -91,6 +107,10 @@ const ProductTemplate = props => {
   const product = data && data.product;
   const site = data && data.site;
 
+  const firstImage = (product.firstImage && product.firstImage.image && product.firstImage.image.asset) && product.firstImage;
+
+  // console.log(product);
+
   if (product.series) {
     product.fullTitle = `${product.series.title} ${product.title}`;
   } else {
@@ -125,7 +145,7 @@ const ProductTemplate = props => {
             fullWidth
             light
           />
-          { product.tearSheet &&
+          { product.tearSheet && product.tearSheet.PDF && product.tearSheet.PDF.asset &&
             <Button
               text="Download Tear Sheet"
               link={product.tearSheet.PDF.asset.url}
@@ -137,14 +157,26 @@ const ProductTemplate = props => {
           }
         </div>
         <div className="col-lg-start-3-span-6 col-md-start-4-span-5">
-          { product.images.map((figure, i) =>
+          { firstImage &&
             <GatsbyImage
-              key={figure._key}
-              image={figure.image.asset.gatsbyImageData}
-              alt={figure.alt}
-              className={cn(i == 0 ? styles.firstImage : '', styles.image)}
+              image={firstImage.image.asset.gatsbyImageData}
+              alt={firstImage.alt}
+              className={cn(styles.firstImage, styles.image)}
               style={{display: 'block'}}
             />
+          }
+          { product.images.map((figure, i) =>
+            <React.Fragment key={figure._key}>
+              { figure.image && figure.image.asset &&
+                <GatsbyImage
+                  key={figure._key}
+                  image={figure.image.asset.gatsbyImageData}
+                  alt={figure.alt}
+                  className={cn(!product.firstImage ? styles.firstImage : '', styles.image)}
+                  style={{display: 'block'}}
+                />
+              }
+            </React.Fragment>
           )}
         </div>
         <div className={cn("col-md-start-9-span-2", styles.productInfo)}>
@@ -157,18 +189,24 @@ const ProductTemplate = props => {
           }
           <br />
           { product.options.length > 0 &&
-            <h3 className="label">Options:</h3>
-          }
-          { product.options && product.options.map((option, i) =>
-            <div key={option._id} className={styles.lightText}>
-              <h3>{option.title}:</h3>
-              {option.types.map((type, i) =>
-                <p key={type._key}>
-                  - {type.title}
-                </p>
+            <>
+              <h3 className="label">Options:</h3>
+              { product.options.map((option, i) =>
+                <React.Fragment key={option && option._id}>
+                  { option &&
+                    <div className={styles.lightText}>
+                      <h3>{option.title}:</h3>
+                      {option.types.map((type, i) =>
+                        <p key={type._key}>
+                          - {type.title}
+                        </p>
+                      )}
+                    </div>
+                  }
+                </React.Fragment>
               )}
-            </div>
-          )}
+            </>
+          }
           { product._rawDimensions &&
             <>
               <br />
