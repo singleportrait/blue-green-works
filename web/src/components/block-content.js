f8c0a21680eb816ebcd3a5e273518d1b610beb3c
Jenn Scheer <singleportrait@gmail.com>
2021-05-15 11:03:31 -0700

    Add margins below paragraphs in rich text blocks

--- web/src/components/block-content.js
+++ web/src/components/block-content.js
@@ -2,10 +2,13 @@ import BaseBlockContent from "@sanity/block-content-to-react";
 import React from "react";
 import clientConfig from "../../client-config";
 import serializers from "./serializers";
+import { cn } from "../lib/helpers";
+
+import * as styles from './blockContent.module.scss';
 
 const BlockContent = ({ blocks = [], className = '' }) => (
   <BaseBlockContent
-    className={className}
+    className={cn(className, styles.blockContent)}
     renderContainerOnSingleChild={true}
     blocks={blocks}
     serializers={serializers}
