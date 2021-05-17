9b68ac0b8eac028f50dc673ff48d83b6db9dea09
Jenn Scheer <singleportrait@gmail.com>
2021-05-17 11:48:16 -0700

    Add mobile header with hamburger menu

---
+++ web/src/components/hamburgerIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HamburgerIcon = (props) => {
+  return (
+    <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line x1="14.75" y1="1.25" x2="-3.22371e-08" y2="1.25" stroke="black"/>
+      <line x1="14.75" y1="7.00012" x2="-4.48515e-08" y2="7.00012" stroke="black"/>
+      <line x1="14.75" y1="4.125" x2="-4.48515e-08" y2="4.125" stroke="black"/>
+      <line x1="14.75" y1="12.75" x2="-4.48515e-08" y2="12.75" stroke="black"/>
+      <line x1="14.75" y1="9.875" x2="-4.48515e-08" y2="9.875" stroke="black"/>
+    </svg>
+  );
+}
+
+export default HamburgerIcon;
--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import { cn } from "../lib/helpers";
 
+import HamburgerIcon from './hamburgerIcon';
 import Logo from './logo';
 import narrowLogo from '../images/blueGreenWorksComingSoonMobile.svg';
 
 import * as styles from "./header.module.scss";
 
-const Header = () => (
-  <div className={styles.header}>
-    <Link to={`/previewHomepage`} className={cn(styles.text, 'label')}>
-      Home
-    </Link>
-    <Link className={styles.logoContainer} to={`/previewHomepage`}>
-      <img src={narrowLogo} alt="Logo" className={styles.narrowLogo} />
-      <Logo className={styles.logo} />
-    </Link>
-    <Link to={`/about`} className={cn(styles.text, 'label')}>
-      About
-    </Link>
-  </div>
-);
+const Header = (props) => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  return (
+    <>
+      <div className={styles.header}>
+        <Link to={`/previewHomepage`} className={cn(styles.text, 'label')}>
+          Home
+        </Link>
+        <Link className={styles.logoContainer} to={`/previewHomepage`}>
+          <img src={narrowLogo} alt="Logo" className={styles.narrowLogo} />
+          <Logo className={cn(styles.logo, styles.wideLogo)} />
+        </Link>
+        <Link to={`/about`} className={cn(styles.text, 'label')}>
+          About
+        </Link>
+      </div>
+      <div className={cn(
+          styles.mobileHeader,
+          showLinks && styles.mobileHeaderOpen
+        )}
+      >
+        <div className={styles.mobileLogoContainer}>
+          <Link to={`/previewHomepage`}>
+            <Logo className={styles.logo} />
+          </Link>
+          <div
+            onClick={() => setShowLinks(!showLinks)}
+            className={styles.hamburgerMenu}
+          >
+            <HamburgerIcon />
+          </div>
+        </div>
+        { showLinks &&
+          <div className={styles.mobileHeaderLinks}>
+            <Link to={`/previewHomepage`} className={cn(styles.text, 'label')}>
+              Home
+            </Link>
+            <Link to={`/about`} className={cn(styles.text, 'label', styles.aboutLink)}>
+              About
+            </Link>
+          </div>
+        }
+      </div>
+    </>
+  )
+};
 
 export default Header;
