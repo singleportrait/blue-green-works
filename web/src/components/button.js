5f05f9502bba933f3c56283d14db8deec084b00c
Jenn Scheer <singleportrait@gmail.com>
2021-05-19 23:39:29 -0400

    Allow internal links to use Gatsby's Link component within the Button component

--- web/src/components/button.js
+++ web/src/components/button.js
@@ -1,23 +1,42 @@
 import React from 'react';
+import { Link } from "gatsby";
+
 import { cn } from '../lib/helpers';
 
-const Button = ({text, link, targetBlank, filled, fullWidth, light, className = ''}) => {
-  return (
-    <a
-      href={link}
-      className={cn(
-        'button',
-        filled && 'button--filled',
-        fullWidth && 'button--fullWidth',
-        light && 'button--light',
-        className
-      )}
-      target={targetBlank && '_blank'}
-      rel={targetBlank && 'noopener noreferrer'}
-    >
-      {text}
-    </a>
-  );
+const Button = ({text, link, targetBlank, filled, fullWidth, light, className = '', internalLink}) => {
+  if (internalLink) {
+    return (
+      <Link
+        to={link}
+        className={cn(
+          'button',
+          filled && 'button--filled',
+          fullWidth && 'button--fullWidth',
+          light && 'button--light',
+          className
+        )}
+      >
+        {text}
+      </Link>
+    );
+  } else {
+    return (
+      <a
+        href={link}
+        className={cn(
+          'button',
+          filled && 'button--filled',
+          fullWidth && 'button--fullWidth',
+          light && 'button--light',
+          className
+        )}
+        target={targetBlank && '_blank'}
+        rel={targetBlank && 'noopener noreferrer'}
+      >
+        {text}
+      </a>
+    );
+  }
 }
 
 export default Button;
