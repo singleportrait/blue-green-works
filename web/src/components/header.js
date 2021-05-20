6dcfb88d413432d16ce58d49e374dfe704aec776
Jenn Scheer <singleportrait@gmail.com>
2021-05-19 23:36:44 -0400

    Add header link to dynamic products page
    Using slug again!

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { StaticQuery, graphql } from "gatsby";
 import { Link } from "gatsby";
 import { cn } from "../lib/helpers";
 
@@ -8,51 +9,76 @@ import narrowLogo from '../images/blueGreenWorksComingSoonMobile.svg';
 
 import * as styles from "./header.module.scss";
 
+const headerQuery = graphql`
+  query DefaultHeaderQuery {
+    productsPage: sanityProductsPage(_id: { regex: "/(drafts.|)productsPage/" }) {
+      title
+      slug {
+        current
+      }
+    }
+  }
+`;
+
 const Header = (props) => {
   const [showLinks, setShowLinks] = useState(false);
 
   return (
-    <>
-      <div className={styles.header}>
-        <Link to={`/previewHomepage`} className={cn(styles.text, 'label')}>
-          Home
-        </Link>
-        <Link className={styles.logoContainer} to={`/previewHomepage`}>
-          <img src={narrowLogo} alt="Logo" className={styles.narrowLogo} />
-          <Logo className={cn(styles.logo, styles.wideLogo)} />
-        </Link>
-        <Link to={`/about`} className={cn(styles.text, 'label')}>
-          About
-        </Link>
-      </div>
-      <div className={cn(
-          styles.mobileHeader,
-          showLinks && styles.mobileHeaderOpen
-        )}
-      >
-        <div className={styles.mobileLogoContainer}>
-          <Link to={`/previewHomepage`}>
-            <Logo className={styles.logo} />
-          </Link>
-          <div
-            onClick={() => setShowLinks(!showLinks)}
-            className={styles.hamburgerMenu}
-          >
-            <HamburgerIcon />
-          </div>
-        </div>
-        { showLinks &&
-          <div className={styles.mobileHeaderLinks}>
-            <Link to={`/previewHomepage`} className={cn(styles.text, 'label')}>
-              Home
-            </Link>
-            <Link to={`/about`} className={cn(styles.text, 'label', styles.aboutLink)}>
-              About
-            </Link>
-          </div>
-        }
-      </div>
-    </>
+    <StaticQuery
+      query={headerQuery}
+      render={data => {
+
+        const productsPage = data.productsPage;
+
+        return (
+          <>
+            <div className={styles.header}>
+              { productsPage.slug && productsPage.slug.current &&
+                <Link to={`/${productsPage.slug.current}`} className={cn(styles.text, 'label')}>
+                  { productsPage.title }
+                </Link>
+              }
+              <Link className={styles.logoContainer} to={`/previewHomepage`}>
+                <img src={narrowLogo} alt="Logo" className={styles.narrowLogo} />
+                <Logo className={cn(styles.logo, styles.wideLogo)} />
+              </Link>
+              <Link to={`/about`} className={cn(styles.text, 'label')}>
+                About
+              </Link>
+            </div>
+            <div className={cn(
+                styles.mobileHeader,
+                showLinks && styles.mobileHeaderOpen
+              )}
+            >
+              <div className={styles.mobileLogoContainer}>
+                <Link to={`/previewHomepage`}>
+                  <Logo className={styles.logo} />
+                </Link>
+                <div
+                  onClick={() => setShowLinks(!showLinks)}
+                  className={styles.hamburgerMenu}
+                >
+                  <HamburgerIcon />
+                </div>
+              </div>
+              { showLinks &&
+                <div className={styles.mobileHeaderLinks}>
+                  { productsPage.slug && productsPage.slug.current &&
+                    <Link to={`/${productsPage.slug.current}`} className={cn(styles.text, 'label')}>
+                      { productsPage.title }
+                    </Link>
+                  }
+                  <Link to={`/about`} className={cn(styles.text, 'label', styles.aboutLink)}>
+                    About
+                  </Link>
+                </div>
+              }
+            </div>
+          </>
+        )
+      }}
+    />
   )
 };
 
