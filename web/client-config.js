6a5db8f121a12cb74980172a1563f7a8f1a4c252
Jenn Scheer <singleportrait@gmail.com>
2022-08-24 17:32:02 +0200

    Update Sanity dataset and improve readme instructions

--- web/client-config.js
+++ web/client-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   sanity: {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'hontxz4x',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'websitev2',
     apiVersion: '2021-04-16',
   }
 }
