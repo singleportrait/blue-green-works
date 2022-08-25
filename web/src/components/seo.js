0700ccec2ccf7cecb24657a6ae20e57db6372e04
Jenn Scheer <singleportrait@gmail.com>
2022-08-25 10:58:35 +0200

    Update Sanity packages, remove prop-types

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -1,15 +1,14 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
+import React from 'react';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
 
-function SEO({ description, lang, meta, keywords, title, imageUrl = '' }) {
+function SEO({ description = '', lang = 'en', meta = [], keywords = [], title = '', imageUrl = '' }) {
   return (
     <StaticQuery
       query={detailsQuery}
       render={data => {
-        const metaDescription = description || (data.site && data.site.seo && data.site.seo.metaDescription) || "";
-        const siteTitle = (data.site && data.site.title) || "";
+        const metaDescription = description || (data.site && data.site.seo && data.site.seo.metaDescription) || '';
+        const siteTitle = (data.site && data.site.title) || '';
         const metaImage = imageUrl ? `${imageUrl}?fm=jpg&w=1200&fit=max` : (data.site && data.site.seo && data.site.seo.openGraphImage && `${data.site.seo.openGraphImage.asset.url}?fm=jpg&w=1200&fit=max`);
 
         // console.log("Data");
@@ -18,48 +17,48 @@ function SEO({ description, lang, meta, keywords, title, imageUrl = '' }) {
           <Helmet
             htmlAttributes={{ lang }}
             title={title}
-            titleTemplate={title === siteTitle ? "%s" : `%s | ${siteTitle}`}
+            titleTemplate={title === siteTitle ? '%s' : `%s | ${siteTitle}`}
             meta={[
               {
-                name: "description",
-                content: metaDescription
+                name: 'description',
+                content: metaDescription,
               },
               {
-                property: "og:title",
-                content: title
+                property: 'og:title',
+                content: title,
               },
               {
-                property: "og:description",
-                content: metaDescription
+                property: 'og:description',
+                content: metaDescription,
               },
               {
-                property: "og:type",
-                content: "website"
+                property: 'og:type',
+                content: 'website',
               },
               {
-                property: "og:image",
-                content: metaImage
+                property: 'og:image',
+                content: metaImage,
               },
               {
-                name: "twitter:card",
-                content: "summary"
+                name: 'twitter:card',
+                content: 'summary',
               },
               {
-                name: "twitter:title",
-                content: title
+                name: 'twitter:title',
+                content: title,
               },
               {
-                name: "twitter:description",
-                content: metaDescription
-              }
+                name: 'twitter:description',
+                content: metaDescription,
+              },
             ]
               .concat(
                 keywords && keywords.length > 0
                   ? {
-                      name: "keywords",
-                      content: keywords.join(", ")
+                      name: 'keywords',
+                      content: keywords.join(', '),
                     }
-                  : []
+                  : [],
               )
               .concat(meta)}
           />
@@ -68,20 +67,6 @@ function SEO({ description, lang, meta, keywords, title, imageUrl = '' }) {
     />
   );
 }
-
-SEO.defaultProps = {
-  lang: "en",
-  meta: [],
-  keywords: []
-};
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.array,
-  keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string.isRequired
-};
 
 export default SEO;
 
