f8b2aea72c474f723287604a49a5bf85ab024b23
Jenn Scheer <singleportrait@gmail.com>
2022-08-29 12:20:33 +0200

    Fix header scrollTrigger cleanup bug when moving in inspector between desktop & mobile layouts

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { StaticQuery, graphql, Link } from 'gatsby';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import React, { useEffect, useRef, useState } from "react";
+import { StaticQuery, graphql, Link } from "gatsby";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
-import { cn } from '../lib/helpers';
+import { cn } from "../lib/helpers";
 
-import HamburgerIcon from './hamburgerIcon';
-import Logo from './logo';
+import HamburgerIcon from "./hamburgerIcon";
+import Logo from "./logo";
 
-import narrowLogo from '../images/blueGreenWorksComingSoonMobile.svg';
+import narrowLogo from "../images/blueGreenWorksComingSoonMobile.svg";
 
-import * as styles from './header.module.scss';
+import * as styles from "./header.module.scss";
 
 const headerQuery = graphql`
   query DefaultHeaderQuery {
@@ -36,99 +36,94 @@ const Header = () => {
 
     let logo;
 
-    mm.add('(min-width:769px)', () => {
+    mm.add("(min-width:769px)", () => {
       logo = gsap.to(logoContainer.current, {
-        scale: 0.70,
+        scale: 0.7,
         y: -20,
         scrollTrigger: {
           start: "top 20px",
           end: "bottom 85%",
           // markers: true,
           scrub: true,
-          toggleActions: 'restart pause restart pause',
+          toggleActions: "restart pause restart pause",
           // onToggle: self => console.log("toggled, isActive:", self.isActive),
-        }
+        },
       });
     });
 
     return () => {
-      logo.scrollTrigger.kill();
-    }
+      logo?.scrollTrigger?.kill();
+    };
   }, [logoContainer]);
 
   return (
     <StaticQuery
       query={headerQuery}
-      render={data => {
-
+      render={(data) => {
         const productsPage = data.productsPage;
 
         return (
           <>
             <div className={styles.header}>
               <div className={cn(styles.linksContainer, styles.leftLinksContainer)}>
-                {productsPage.slug && productsPage.slug.current &&
-                  <Link to={`/${productsPage.slug.current}`} className={cn(styles.text, 'label')}>
+                {productsPage.slug && productsPage.slug.current && (
+                  <Link to={`/${productsPage.slug.current}`} className={cn(styles.text, "label")}>
                     {productsPage.title}
                   </Link>
-                }
-                <Link to={'/about'} className={cn(styles.text, 'label')}>
+                )}
+                <Link to={"/about"} className={cn(styles.text, "label")}>
                   About
                 </Link>
               </div>
-              <Link ref={logoContainer} className={styles.logoContainer} to={'/'}>
+              <Link ref={logoContainer} className={styles.logoContainer} to={"/"}>
                 <img src={narrowLogo} alt="Logo" className={styles.narrowLogo} />
                 <Logo className={cn(styles.logo, styles.wideLogo)} />
               </Link>
               <div className={cn(styles.linksContainer, styles.rightLinksContainer)}>
-                <Link to={'/press'} className={cn(styles.text, 'label')}>
+                <Link to={"/press"} className={cn(styles.text, "label")}>
                   Press
                 </Link>
-                <Link to={'/press'} className={cn(styles.text, 'label')}>
+                <Link to={"/press"} className={cn(styles.text, "label")}>
                   Book
                 </Link>
               </div>
             </div>
-            <div className={cn(
-              styles.mobileHeader,
-              showLinks && styles.mobileHeaderOpen,
-            )}
-            >
+            <div className={cn(styles.mobileHeader, showLinks && styles.mobileHeaderOpen)}>
               <div className={styles.mobileLogoContainer}>
-                <Link to={'/'}>
+                <Link to={"/"}>
                   <Logo className={styles.logo} />
                 </Link>
-                <div
-                  onClick={() => setShowLinks(!showLinks)}
-                  className={styles.hamburgerMenu}
-                >
+                <div onClick={() => setShowLinks(!showLinks)} className={styles.hamburgerMenu}>
                   <HamburgerIcon />
                 </div>
               </div>
-              {showLinks &&
+              {showLinks && (
                 <div className={styles.mobileHeaderLinks}>
-                  {productsPage.slug && productsPage.slug.current &&
-                    <Link to={`/${productsPage.slug.current}`} className={cn(styles.text, 'label', styles.firstLink)}>
+                  {productsPage.slug && productsPage.slug.current && (
+                    <Link
+                      to={`/${productsPage.slug.current}`}
+                      className={cn(styles.text, "label", styles.firstLink)}
+                    >
                       {productsPage.title}
                     </Link>
-                  }
-                  <Link to={'/about'} className={cn(styles.text, 'label')}>
+                  )}
+                  <Link to={"/about"} className={cn(styles.text, "label")}>
                     About
                   </Link>
-                  <Link to={'/press'} className={cn(styles.text, 'label')}>
+                  <Link to={"/press"} className={cn(styles.text, "label")}>
                     Press
                   </Link>
-                  <Link to={'/press'} className={cn(styles.text, 'label', styles.lastLink)}>
+                  <Link to={"/press"} className={cn(styles.text, "label", styles.lastLink)}>
                     Book
                   </Link>
                 </div>
-              }
+              )}
             </div>
           </>
-        )
+        );
       }}
     />
-  )
+  );
 };
 
 export default Header;
