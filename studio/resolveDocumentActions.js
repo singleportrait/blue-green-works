da86cf063eb0ce28a5f360e7ac5230f1fd4a75f0
Jenn Scheer <singleportrait@gmail.com>
2022-09-23 18:24:10 -0400

    Remove old Gatsby dashboard plugin and add new preview plugin

---
+++ studio/resolveDocumentActions.js
@@ -0,0 +1,5 @@
+import defaultResolve from 'part:@sanity/base/document-actions'
+import {gatsbyPreviewAction} from 'sanity-plugin-gatsby-cloud-preview'
+export default function resolveDocumentActions (props) {
+  return [...defaultResolve(props), gatsbyPreviewAction]
+}
